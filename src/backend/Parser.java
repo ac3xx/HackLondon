c1a5e916742173a12d370773ad22157eefe24b22
jamesprinc3 <Ecnirp99>
2015-03-01 06:13:21 +0000

    buttfuck

--- src/backend/Parser.java
+++ src/backend/Parser.java
@@ -21,59 +21,67 @@ public class Parser {
     }
 
     public void parse(String string) {
-        scanner = new Scanner(string).useDelimiter("\n(?<=[;}])");
+        scanner = new Scanner(string).useDelimiter("(?<=[;}\n])");
         // everything starts with var, control or "//"
-        try {
-            String stmt = scanner.next();
-            char lastChar = stmt.substring(stmt.length() - 1).toCharArray()[0];
-            System.out.println("lastchar: " + lastChar + " stmt: " + stmt);
-            if (lastChar == '}') {
-                //it's control flow
-                Scanner stmtScan = new Scanner(stmt);
-                String name = stmtScan.next();
-                switch (name) {
-                    case "if":
-                        String condition = stmtScan.next();
-                        stripBrackets(condition);; // strip the brackets
-                        String statements = stmtScan.next();
-                        stripBrackets(statements); //strip curly braces
-                        Scanner blockScanner = new Scanner(statements).useDelimiter("\n(?<=;)");
-                        Block trueBlock = new Block();
-                        while (blockScanner.hasNext()) {
-                            trueBlock.addStatement(blockScanner.next());
-                        }
-                        Block falseBlock = null;
-                        if (scanner.hasNext("else") || scanner.hasNext(" else")) {
-                            stmt = scanner.next(); //drop the else
-                            stmtScan = new Scanner(stmt); stmtScan.next(); //drop the else
-                            statements = stmtScan.next();
+        while (scanner.hasNext()) {
+            try {
+                String stmt = scanner.next();
+                char lastChar = stmt.substring(stmt.length() - 1).toCharArray()[0];
+                System.out.println("lastchar: " + lastChar + " stmt: " + stmt);
+                if (lastChar == '}') {
+                    //it's control flow
+                    Scanner stmtScan = new Scanner(stmt).useDelimiter("(?<=[)])");
+                    String name = stmtScan.next();
+                    switch (name) {
+                        case "if":
+                            String condition = stmtScan.next();
+                            stripBrackets(condition);
+                            ; // strip the brackets
+                            String statements = stmtScan.next();
                             stripBrackets(statements); //strip curly braces
-                            blockScanner = new Scanner(statements).useDelimiter("\n(?<=;)");
-                            falseBlock = new Block();
+                            Scanner blockScanner = new Scanner(statements).useDelimiter("(?<=[;\n])");
+                            Block trueBlock = new Block();
                             while (blockScanner.hasNext()) {
-                                falseBlock.addStatement(blockScanner.next());
+                                trueBlock.addStatement(blockScanner.next());
                             }
-                        }
-                        If ifcontrol = new If(scope, condition, trueBlock, falseBlock);
-                        break;
-                    case "for":
-                        //TODO: Make for class
-                        break;
-                    case "while":
-                        //TODO: Make while class
-                        break;
-                    default:
-                        break;
+                            Block falseBlock = null;
+                            if (scanner.hasNext("else") || scanner.hasNext(" else")) {
+                                stmt = scanner.next(); //drop the else
+                                stmtScan = new Scanner(stmt);
+                                stmtScan.next(); //drop the else
+                                statements = stmtScan.next();
+                                stripBrackets(statements); //strip curly braces
+                                blockScanner = new Scanner(statements).useDelimiter("(?<=[;\n])");
+                                falseBlock = new Block();
+                                while (blockScanner.hasNext()) {
+                                    falseBlock.addStatement(blockScanner.next());
+                                }
+                            }
+                            If ifcontrol = new If(scope, condition, trueBlock, falseBlock);
+                            ifcontrol.execute();
+
+                            break;
+                        case "for":
+                            //TODO: Make for class
+                            break;
+                        case "while":
+                            //TODO: Make while class
+                            break;
+                        default:
+                            break;
+                    }
+                } else if (lastChar == ';') {
+                    //it's a var, execute the fucker
+                    System.out.println("stmt execute: " + stmt);
+//                    statementExecutor.execute(stmt);
                 }
-            } else if (lastChar == ';') {
-                //it's a var, execute the fucker
-                System.out.println("stmt execute: "+ stmt);
-                statementExecutor.execute(stmt);
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+//            } catch (StatementException e) {
+//                e.printStackTrace();
+            } catch (StatementException e) {
+                e.printStackTrace();
             }
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-        } catch (StatementException e) {
-            e.printStackTrace();
         }
     }
 
